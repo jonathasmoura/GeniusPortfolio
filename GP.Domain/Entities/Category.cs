e423e714b26e85d8027fc7be26650d60b3964b10
Jonathas Moura <jonathas.development@gmail.com>
2025-08-16 15:26:24 -0300

    Criação estrutura de domínio
    Realizado criação de entidades e interfaces, evoluindo estrutura inicial do projeto.

---
+++ GP.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using GP.Domain.Entities.DomainBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP.Domain.Entities
+{
+	public class Category : EntityBase
+	{
+		public string Name { get; set; } = null!;
+		public string Description { get; set; } = null!;
+
+	
+	}
+}
